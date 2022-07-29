43946d1471f1aa235a2b62e79ae6f2f8b55e323f
San-43 <virgosmp.mx@gmail.com>
2022-07-28 23:07:38 -0500

    :)

--- main.cpp
+++ main.cpp
@@ -1,29 +1,34 @@
 #include <istream>
 #include <cmath>
-#include "/libcs50-11.0.1/src/cs50.h"
 
 using namespace std;
 
 int main() {
 
-    double a = get_double("Enter coefficient a: ");
-    double b = get_double("Enter coefficient b: ");
-    double c = get_double("Enter coefficient c: ");
-
+    double a;
+    double b;
+    double c;
     double d;
 
-    d = pow(b, 2) - 4 * a * c;
+    printf("Enter coefficient a: ");
+    scanf("%lf", &a);
+    printf("Enter coefficient b: ");
+    scanf("%lf", &b);
+    printf("Enter coefficient c: ");
+    scanf("%lf", &c);
+
+    d = pow(b, 2) - (4 * a * c);
 
     if (d > 0) {
-        double x1 = (-b + sqrt(d)) / (2 * a);
-        double x2 = (-b - sqrt(d)) / (2 * a);
+        double x1 = (b * -1 + sqrt(d)) / (2 * a);
+        double x2 = (b * -1 - sqrt(d)) / (2 * a);
 
-        printf("Two solutions: %d and %d ", x1, x2);
+        printf("Two solutions: %f and %f", x1, x2);
     } else if (d < 0) {
         d = d * -1;
         d = sqrt(d) / (2 * a);
-        printf("Two complex solutions: %d + %d%s and %d - %d%s", -b/(2 * a), d, "i", -b/(2 * a), d, "i");
+        printf("Two complex solutions: %f + %f%s and %f - %f%s", -b/(2 * a), d, "i", -b/(2 * a), d, "i");
     } else {
-        printf("One solution: %d", -b / (2 * a));
+        printf("One solution: %f", (b * -1)/(2 * a));
     }
 }
